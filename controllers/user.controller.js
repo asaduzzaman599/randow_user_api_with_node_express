242f5126f3c1a7268f575dcd021e0ecae3a06168
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-09-02 23:24:24 +0600

    bonus done

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -17,7 +17,6 @@ module.exports.getAllUser =async(req,res)=>{
     const users = await userDataFile.getUsersData()
     const length =users.length
     const index = Math.floor(Math.random()*length)
-    console.log(index)
     const user = users[index]
     res.send({"udataser":user})
 }
